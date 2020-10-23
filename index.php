d661e59ce59ec4193bf451960606de9a5919a660
Jeremy Mordo <66905407+JeremyMordo@users.noreply.github.com>
2020-10-23 19:58:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
 <?php
 
 require 'Vehicle.php';
-require 'HighWay.php';
-
-$MotorWay = new MotorWay(4, 130);
-$PedestrianWay = new PedestrianWay(1, 10);
-$ResidentialWay = new ResidentialWay(2, 50);
 
 $Car = new Car('red', 2, 1);
-$Bike = new Bike ('blue', 3, 1);
-$SkateBoard = new Skateboard('black', 4, 3);
 
-echo "1)";
-$MotorWay->addVehicle($Car)."<br/>";
-echo "2)";
-$MotorWay->addVehicle($Bike)."<br/>";
-echo "3)";
-$MotorWay->addVehicle($SkateBoard)."<br/>";
+echo 'Try n°1 : Park Brake unlock !';
+echo '<br/>';
 
-echo "4)";
-$PedestrianWay->addVehicle($Car)."<br/>";
-echo "5)";
-$PedestrianWay->addVehicle($Bike)."<br/>";
-echo "6)";
-$PedestrianWay->addVehicle($SkateBoard)."<br/>";
+try 
+{
+    $Car->start(true);
+} 
+catch(Exception $e)
+{
+    $Car->setHasParkBrake(true);
+}
+finally
+{
+    echo '<br/>';
+    echo 'Ma voiture roule comme un donut';
+    echo '<br/>';
+}
 
-echo "7)";
-$ResidentialWay->addVehicle($Car)."<br/>";
-echo "8)";
-$ResidentialWay->addVehicle($Bike)."<br/>";
-echo "9)";
-$ResidentialWay->addVehicle($SkateBoard)."<br/>";
+echo '<br/>';
+echo 'Try n°2 : Park Brake lock !';
+echo '<br/>';
+  
 
-
-try
+try 
 {
     $Car->start(false);
-}
-catch (Exception $e)
+} 
+catch(Exception $e)
 {
-    echo "Exception received : " . $e->getmessage();
+    $Car->setHasParkBrake(true);
+    echo 'Park Brake Unlock ! <br/>';
+}
+finally
+{
+    echo 'Ma voiture roule comme un donut';
 }
